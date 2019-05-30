be58d47d7b8908c9710a1fb2fbb7b10f34d94714
skylar80004 <josedavidvm04@hotmail.com>
2019-05-29 19:44:37 -0600

    Primera parte de mostrar amigos

--- app/src/main/java/com/example/proyectofb/FriendFragment.java
+++ app/src/main/java/com/example/proyectofb/FriendFragment.java
@@ -3,10 +3,24 @@ package com.example.proyectofb;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
@@ -64,7 +78,35 @@ public class FriendFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friend, container, false);
+        View view = inflater.inflate(R.layout.fragment_friend, container, false);
+
+        RecyclerView recyclerView  = view.findViewById(R.id.recyclerViewFriendFragment);
+        UserThumbnailAdapter adapter = new UserThumbnailAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(adapter);
+
+        Button buttonFriendsAll = view.findViewById(R.id.buttonFriendsAll);
+        buttonFriendsAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // Buscar todos los amigos
+
+            }
+        });
+        return view;
+    }
+
+    public void FirebaseGetAllFriendsCurrentUser(){
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        String id = user.getUid();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference reference = database.getReference();
+
+        //FALTA
+
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
