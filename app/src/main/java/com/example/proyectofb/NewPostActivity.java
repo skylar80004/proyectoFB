161ba3dda4dfa559fb790968c72af402cdd65172
skylar80004 <josedavidvm04@hotmail.com>
2019-06-04 10:34:14 -0600

    Realizar publicaciones simples, tipo texto

--- app/src/main/java/com/example/proyectofb/NewPostActivity.java
+++ app/src/main/java/com/example/proyectofb/NewPostActivity.java
@@ -1,6 +1,10 @@
 package com.example.proyectofb;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +15,21 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.sql.SQLTransactionRollbackException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class NewPostActivity extends AppCompatActivity {
 
@@ -25,8 +44,18 @@ public class NewPostActivity extends AppCompatActivity {
         RadioButton radioButtonText =  findViewById(R.id.radioButtonTextOption);
         radioButtonText.setChecked(true);
         this.setInitialRadioButton();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
     }
+
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return false;
+    }
+
 
 
     public void setInitialRadioButton(){
@@ -42,15 +71,103 @@ public class NewPostActivity extends AppCompatActivity {
 
     }
 
+    public Map<String, Object> getPostMap(String name, String lastName,String type,
+                                          String text,String imageUrl,String profilePhotoUrl){
+        Map<String,Object> postMap = new HashMap<>();
+        postMap.put("name",name);
+        postMap.put("lastName",lastName);
+        postMap.put("type",type);
+        postMap.put("likes", "0");
+        postMap.put("dislikes","0");
+        postMap.put("text",text);
+        postMap.put("imageUrl", imageUrl);
+        postMap.put("profilePhotoUrl",profilePhotoUrl );
+
+        return postMap;
+
+
+    }
+
+    public void FirebaseWriteNewTextPost(final String text){
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        final DatabaseReference databaseReference  = firebaseDatabase.getReference();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        final String userId = user.getUid();
+        final String newPostId = UUID.randomUUID().toString();
+
+
+
+
+        // Obtener datos del usuario que realiza el post
+
+        final UserBasicData userBasicData = new UserBasicData();
+        databaseReference.child("users").orderByKey().addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                String temporalUserKey = dataSnapshot.getKey();
+                if(temporalUserKey.equals(userId)){
+
+                    Map<String,Object> userMap = (Map<String, Object>) dataSnapshot.getValue();
+
+                    String name = (String)userMap.get("name");
+                    String lastName = (String)userMap.get("lastName");
+                    String profilePhotoUrl = (String)userMap.get("profilePhotoUrl");
+
+                    Map<String, Object> postMap = getPostMap(name, lastName,"text",text,"nullvalue",profilePhotoUrl);
+                    databaseReference.child("posts").child(userId).child(newPostId).setValue(postMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(), "Se ha realizado la publicación", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+
+
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+
+    }
+
     public void OnClickButtonMakePost(View view){
 
         EditText editTextNewPostText = findViewById(R.id.editTextNewPostText);
         String newPostText = editTextNewPostText.getText().toString();
-
         int radioButton = radioGroup.getCheckedRadioButtonId();
         switch (radioButton){
             case R.id.radioButtonTextOption:
-                Toast.makeText(this,newPostText,Toast.LENGTH_LONG).show();
+
+
+                this.FirebaseWriteNewTextPost(newPostText);
                 break;
         }
 
---
+++ app/src/main/java/com/example/proyectofb/UserBasicData.java
@@ -0,0 +1,36 @@
+package com.example.proyectofb;
+
+public class UserBasicData {
+
+    String userName;
+    String userLastName;
+    String profilePhotoUrl;
+
+    public UserBasicData(){
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserLastName() {
+        return userLastName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
+    }
+
+    public String getProfilePhotoUrl() {
+        return profilePhotoUrl;
+    }
+
+    public void setProfilePhotoUrl(String profilePhotoUrl) {
+        this.profilePhotoUrl = profilePhotoUrl;
+    }
+}
