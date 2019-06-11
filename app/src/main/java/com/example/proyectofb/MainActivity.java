1ebf9a651522f52f1764b099f25255f089a3faeb
skylar80004 <josedavidvm04@hotmail.com>
2019-06-11 00:21:32 -0600

    Login Basico(sin obtener informacion del usuario) de google listo

--- app/src/main/java/com/example/proyectofb/MainActivity.java
+++ app/src/main/java/com/example/proyectofb/MainActivity.java
@@ -4,11 +4,17 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -19,14 +25,38 @@ public class MainActivity extends AppCompatActivity {
 
 
     private FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
     private int SingUpCode = 4;
+    int RC_SIGN_IN = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                GoogleSignIn();
+            }
+        });
+
+
     }
 
+
+    public void GoogleSignIn(){
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
     public void OnClickButtonSignUp(View view){
 
         Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
@@ -114,5 +144,39 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Toast.makeText(getApplicationContext(), "Login Exitoso" ,Toast.LENGTH_LONG).show();
+            Intent intent  = new Intent(getApplicationContext(),TabHomeActivity.class);
+            startActivity(intent);
+
+
+        } catch (ApiException e) {
+
+            Log.d("CHES", "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(getApplicationContext(), "No fue posible iniciar sesion.",Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+
 
 }
